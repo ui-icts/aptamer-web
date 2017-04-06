545605f5c00b5fdc742ee3bc3435fbc15bbd9f3c
Chris Ortman <chris-ortman@uiowa.edu>
2017-04-05 22:23:50 -0500

    Add file upload component

--- frontend/app/components/graph-options-form.js
+++ frontend/app/components/graph-options-form.js
@@ -3,4 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showHelp: false,
   edgeTypes: ['edit', 'tree', 'both']
+
 });
--- frontend/app/routes/create-graph.js
+++ frontend/app/routes/create-graph.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addedFile(x) {
+      console.log("Added file", arguments);
+    },
+
+    successFile(x) {
+      console.log("Success File", arguments);
+    },
+
+    completeFile(x) {
+      console.log("Complete FIle", arguments);
+
+    },
+
+    errorFile(x) {
+      console.log("Error File", arguments);
+    }
+  }
 });
