a7ac564d896b1a6550e1a56d8e94b04ef936a086
Chris Ortman <chris-ortman@uiowa.edu>
2017-04-06 09:16:24 -0500

    Adding list of uploaded files

--- frontend/app/routes/create-graph.js
+++ frontend/app/routes/create-graph.js
@@ -1,9 +1,23 @@
 import Ember from 'ember';
 import CreateGraphOptions from '../models/create-graph-options';
 
+let files = [{
+  id: 'final_rd12_struct.fa - 1',
+  fileName: 'Final_Rd12_struct.fa',
+  uploadedOn: '2017-03-25T08:15:30-05:00'
+}, {
+  id: 'final_rd12_struct.fa - 2',
+  fileName: 'Final_Rd12_struct.fa',
+  uploadedOn: '2017-04-01T08:15:30-05:00'
+}];
+
+
 export default Ember.Route.extend({
   model() {
-    return CreateGraphOptions.create();
+    return Ember.Object.create({
+      options: CreateGraphOptions.create(),
+      structureFiles: files
+    });
   }
 
 });
