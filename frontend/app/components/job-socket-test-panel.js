176220e5ad14a11542c2960920020c5ed5fde2c9
Chris Ortman <chris-ortman@uiowa.edu>
2017-05-25 11:22:01 -0500

    Able to send a message and receive a reply and / or broadcast

--- frontend/app/components/job-socket-test-panel.js
+++ frontend/app/components/job-socket-test-panel.js
@@ -20,13 +20,24 @@ export default Ember.Component.extend({
       let socket = new Socket('ws://localhost:4000/socket', {
           logger: ((kind, msg, data) => {
                 console.log(`${kind}: ${msg}`, data);
-                log(`${kind}: ${msg}`);
               })
       });
+
+      socket.onClose( e => log("Closing socket...."));
 
       this.set('socket', socket)
       socket.connect();
       let channel = socket.channel("jobs:status", {});
+
+      channel.on("start_job", payload => {
+        log(`Received start_job broadcast: ${payload.body}`);
+      });
+
+      channel.onError( e => {
+        log("Got error on channel");
+        console.log("Channel Error", e);
+      });
+
       channel.join()
         .receive("ok", resp => {
           log("Connected to jobs:status channel");
@@ -36,6 +47,24 @@ export default Ember.Component.extend({
           log("Error connecting to jobs:status");
           console.log("ER Resp", resp);
         });
-    }
+      this.set('channel', channel);
+    },
+
+    reset() {
+      let channel = this.get('channel');
+      if ( channel ) {
+        channel.leave();
+      }
+    },
+
+    startJob() {
+      let channel = this.get('channel');
+      if ( channel ) {
+        this.log("Sending start_job");
+        channel.push("start_job", {body: "Test job"}).receive("ok", (reply) => {
+          this.log(`Received job_started reply: ${reply.body}`);
+        });
+      }
+    },
   }
 });
