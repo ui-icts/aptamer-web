24965e6d94c6d9363c7dfd01dc0c58bbf59287d4
Chris Ortman <chris-ortman@uiowa.edu>
2017-05-25 11:22:29 -0500

    Can receive start_job and reply / broadcast

--- backend/web/channels/jobs_channel.ex
+++ backend/web/channels/jobs_channel.ex
@@ -2,7 +2,11 @@ defmodule Aptamer.JobsChannel do
   use Phoenix.Channel
 
   def join("jobs:status", _message, socket) do
-    {:ok, socket}
+    {:ok,"Welcome to the jobs status channel", socket}
   end
 
+  def handle_in("start_job", %{"body" => body}, socket) do
+    broadcast! socket, "start_job", %{body: body}
+    {:reply,{:ok, %{body: body}}, socket}
+  end
 end
