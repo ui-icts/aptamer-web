b9bdf1ceeb8e2b726b4f4386646c6d2d473ea09a
Chris Ortman <chris-ortman@uiowa.edu>
2017-06-06 13:59:15 -0500

    Changing file type concept to command

--- frontend/app/components/file-view.js
+++ frontend/app/components/file-view.js
@@ -15,10 +15,26 @@ export default Ember.Component.extend({
     return this.get('fileTypes').list()
   }),
 
+  selectedCommand: Ember.computed('fileType', function() {
+    let ft = this.get('fileType');
+    if ( ft === 'fasta' ) {
+      return 'predict_structures';
+    } else {
+      return 'create_graph';
+    }
+  }),
 
   actions: {
     toggleShowMore() {
       this.toggleProperty('showMore');
+    },
+
+    selectCommand(newCommand) {
+      if ( newCommand === 'predict_structures' ) {
+        this.get('onFileTypeChange')('fasta');
+      } else {
+        this.get('onFileTypeChange')('structure');
+      }
     },
   }
 });
