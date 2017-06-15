1d9f0f8e88f3bdbe7175284e685b3404ef330e7f
Chris Ortman <chris-ortman@uiowa.edu>
2017-06-15 15:21:21 -0500

    Need to make it so that ember will use /subdir for production

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,4 +1,9 @@
 import DS from 'ember-data';
+import ENV from 'your-application-name/config/environment';
+var opts = {};
 
-export default DS.JSONAPIAdapter.extend({
-});
+if ( ENV.environment === 'production' ) {
+  opts['namespace'] = '/aptamer2';
+}
+
+export default DS.JSONAPIAdapter.extend(opts);
