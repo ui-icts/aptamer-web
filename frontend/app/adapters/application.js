3f8eaf045947eebafcb4cde3b78fe7f93d9ab497
Chris Ortman <chris-ortman@uiowa.edu>
2017-06-16 10:11:51 -0500

    Problem with adapter after removing vdir

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,9 +1,15 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 import ENV from 'aptamer/config/environment';
+
 var opts = {};
 
-if ( ENV.environment === 'production' ) {
-  opts['namespace'] = ENV.rootURL;
+if ( ENV.environment === 'production') {
+  //You don't want to set this to just '/' or the 
+  //adapter will request things like http://files?include
+  if ( !Ember.isBlank(ENV.rootURL) && ENV.rootURL !== '/') {
+    opts['namespace'] = ENV.rootURL;
+  }
 }
 
 export default DS.JSONAPIAdapter.extend(opts);
