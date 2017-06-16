33d75bc1d31ef71fce3ee068cd7fddd876bf472c
Chris Ortman <chris-ortman@uiowa.edu>
2017-06-15 20:45:09 -0500

    Found some more ember urls that need to know about rootURL

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import ENV from 'aptamer/config/environment';
+
+Ember.Controller.reopen({
+    rootURL: ENV.rootURL,
+});
+
+Ember.Component.reopen({
+    rootURL: ENV.rootURL,
+});
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
