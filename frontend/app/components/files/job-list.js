175ec860bdf6e27cee1ef5e934d8368d7fdbfdae
Chris Ortman <chris-ortman@uiowa.edu>
2017-06-26 14:49:53 -0500

    Fix job sorting, add logging config

--- frontend/app/components/files/job-list.js
+++ frontend/app/components/files/job-list.js
@@ -1,11 +1,33 @@
 import Ember from 'ember';
+import _ from 'lodash';
 
 function byInserted(job1, job2) {
-  return job2.get('insertedAt') - job1.get('insertedAt');
+  return job1.get('insertedAt') - job2.get('insertedAt');
+}
+
+function momentSort(field) {
+  return function(a,b) {
+    let af = a.get(field),
+        bf = b.get(field);
+
+    if ( af && bf ) {
+      return a.get(field) - b.get(field);
+    } else if ( af ) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
 }
 
 export default Ember.Component.extend({
 
-  orderedJobs: Ember.computed.sort('jobs',byInserted),
+  orderedJobs: Ember.computed('jobs.[]', function() {
+    let jobs = this.get('jobs').toArray();
+    return _(jobs)
+      .sort(momentSort('insertedAt'))
+      .value();
+
+  }),
 
 });
