2a7e9fb16f6bc1571ee138b5f4506b00f60f9e2f
Sam Martinez <samanatha-martinez@uiiowa.edu>
2017-10-05 17:37:08 -0500

    Shows file contents when user clicks on file

--- frontend/app/routes/files/view.js
+++ frontend/app/routes/files/view.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  fileContents: Ember.inject.service(),
+
+  beforeModel() {
+    let fileContents = this.get('fileContents');
+    if ( !fileContents.get('isHealthy') ) {
+      fileContents.connect();
+    }
+  },
+
   model(params) {
     return this.get('store').findRecord('file', params.file_id);
-  }
+  },
+
+   resetController(_controller, _isExiting, _transition) {
+     let fileContents = this.get('fileContents');
+     fileContents.stopCurrentCapture();
+   }
 });
