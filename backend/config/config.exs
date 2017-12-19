de88b4dc6d0e59dc015ccf79ba86c495aa0b7701
Chris Ortman <chris-ortman@uiowa.edu>
2017-12-19 14:37:48 -0600

    Configure SMTP email

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -20,11 +20,11 @@ config :aptamer, Aptamer.Endpoint,
 
 config :aptamer, Aptamer.Mailer,
   adapter: Bamboo.SMTPAdapter,
-  server: "localhost", #SMTP server here
-  hostname: "aptamer.icts.uiowa.edu",
-  # username: {:system, "SMTP_USERNAME"},
-  # password: {:system, "SMTP_PASSWORD"},
-  port: 1025,
+  server: "smtp.office365.com", #SMTP server here
+  hostname: "aptamer-dev.icts.uiowa.edu",
+  username: "ICTS-aptamer-mailer",
+  password: {:system, "SMTP_PASSWORD"},
+  port: 587,
   tls: :if_available, # can be `:always` or `:never`
   allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"], # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
   ssl: false, # can be `true`
