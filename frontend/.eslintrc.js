75269444a5ff84eb784629ae9252ac3e240ad0fc
Chris Ortman <chris-ortman@uiowa.edu>
2019-01-24 09:54:47 -0600

    Update to ember 3.1

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
       files: [
         'ember-cli-build.js',
         'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
