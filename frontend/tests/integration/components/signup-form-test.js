1a59f1edee057babeba7412e486686b559d5019e
Chris Ortman <chris-ortman@uiowa.edu>
2019-01-24 12:55:40 -0600

    Run code mods 3.5

--- frontend/tests/integration/components/signup-form-test.js
+++ frontend/tests/integration/components/signup-form-test.js
@@ -1,6 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { fillIn, click, render, find, settled } from '@ember/test-helpers';
+import {
+  fillIn,
+  click,
+  render,
+  find,
+  settled
+} from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import {
   assertionInjector,
