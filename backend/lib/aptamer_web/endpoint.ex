16f5298b6f3f78de565f413e97ffd899436a54ab
Chris Ortman <chris-ortman@uiowa.edu>
2019-01-25 11:49:38 -0600

    Prod / package updates

--- backend/lib/aptamer_web/endpoint.ex
+++ backend/lib/aptamer_web/endpoint.ex
@@ -10,7 +10,7 @@ defmodule AptamerWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :aptamer, gzip: true
+    at: "/", from: :aptamer, gzip: false
     # only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
