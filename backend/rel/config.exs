6ae3df139d9f0ff8becd37be346212b5dc7bedd6
Chris Ortman <chris-ortman@uiowa.edu>
2019-03-01 11:49:36 -0600

    Re-run release.init

--- backend/rel/config.exs
+++ backend/rel/config.exs
@@ -2,7 +2,8 @@
 # They can then be used by adding `plugin MyPlugin` to
 # either an environment, or release definition, where
 # `MyPlugin` is the name of the plugin module.
-Path.join(["rel", "plugins", "*.exs"])
+~w(rel plugins *.exs)
+|> Path.join()
 |> Path.wildcard()
 |> Enum.map(&Code.eval_file(&1))
 
@@ -13,7 +14,7 @@ use Mix.Releases.Config,
     default_environment: :dev
 
 # For a full list of config options for both releases
-# and environments, visit https://hexdocs.pm/distillery/configuration.html
+# and environments, visit https://hexdocs.pm/distillery/config/distillery.html
 
 
 # You may define one or more environments in this file,
@@ -22,6 +23,12 @@ use Mix.Releases.Config,
 # and environment configuration is called a profile
 
 environment :dev do
+  # If you are running Phoenix, you should make sure that
+  # server: true is set and the code reloader is disabled,
+  # even in dev mode.
+  # It is recommended that you build with MIX_ENV=prod and pass
+  # the --env flag to Distillery explicitly if you want to use
+  # dev mode.
   set dev_mode: true
   set include_erts: false
   set cookie: :"o50JgBb:y%RKHIZ*J8Nb~sKN|rwr7~4*0MGP5<>L^Hb/Vn7m(oj1o{Q*B5NvVzdC"
@@ -30,7 +37,8 @@ end
 environment :habitat do
   set include_erts: true
   set include_src: false
-  set cookie: :"qsd<Ntl,YC{3oWegv;w>h,kqND`0W3a$Hl?_kCIp*MK4/IO!drEm%}/BY]IL9ot<"
+  set cookie: :"_Uz&>WTMY5^fvDCa{)GvCenIk30tq1|}r3T(wAUzK2h1HcM3y|3L2`NaKFp$Z]2>"
+  set vm_args: "rel/vm.args"
 end
 
 # You may define one or more releases in this file.
