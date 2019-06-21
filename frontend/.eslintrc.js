75ccbe68e98185c408241896d7b0ad03bce6ff6a
Chris Ortman <chris-ortman@uiowa.edu>
2019-06-21 09:08:54 -0500

    Update ember to newest
    Trying to get rid of depenency that is triggering a
    security warning

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,7 @@ const isTestEnv = process.env.EMBER_ENV === 'test';
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module'
   },
   plugins: [
@@ -30,7 +30,8 @@ module.exports = {
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
+        'server/**/*.js'
       ],
       parserOptions: {
         sourceType: 'script',
@@ -39,7 +40,15 @@ module.exports = {
       env: {
         browser: false,
         node: true
-      }
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off'
+      })
     }
   ]
 };
