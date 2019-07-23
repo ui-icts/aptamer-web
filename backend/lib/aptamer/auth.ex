b5d91d8668cb0783103a9cd299ed2e94bca7d4a6
Chris Ortman <chris-ortman@uiowa.edu>
2019-07-23 14:02:38 -0500

    Adding auth helpers

---
+++ backend/lib/aptamer/auth.ex
@@ -0,0 +1,32 @@
+defmodule Aptamer.Auth do
+  @moduledoc """
+  The Auth context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Aptamer.Repo
+
+  alias Aptamer.Auth.User
+
+  def list_users() do
+    Repo.all(User)
+    |> Enum.map(fn u ->
+      Map.take(u, [:name, :email, :updated_at])
+    end)
+  end
+
+  def change_password(email_address, new_password) do
+
+    user =
+      User
+      |> where(email: ^email_address)
+      |> Repo.one!()
+
+    cs = User.changeset(user, %{password: new_password})
+    if cs.valid? do
+      Repo.update(cs)
+    else
+      {:error, cs}
+    end
+  end
+end
